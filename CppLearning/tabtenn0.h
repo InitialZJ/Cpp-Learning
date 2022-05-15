39ebcd422d9f0c0e93ad743479d325f1eed2b7a7
Jeffrey0418 <blueskyzzz0418@gmail.com>
2022-05-15 13:22:44 +0800

    新增类的继承

---
+++ CppLearning/tabtenn0.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+using std::string;
+
+class TableTennisPlayer
+{
+private:
+	string firstname;
+	string lastname;
+	bool hasTable;
+public:
+	TableTennisPlayer(const string& fn = "none",
+		const string& ln = "none", bool ht = false);
+	void Name() const;
+	bool HasTable() const { return hasTable; };
+	void ResetTable(bool v) { hasTable = v; };
+};
