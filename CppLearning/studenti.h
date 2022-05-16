583a4f88d2be1ff04d45e93a6b0ab1b2e592e536
Jeffrey0418 <blueskyzzz0418@gmail.com>
2022-05-16 16:33:13 +0800

    新增私有继承、多继承

---
+++ CppLearning/studenti.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <valarray>
+
+class Student : private std::string, private std::valarray<double>
+{
+private:
+	typedef std::valarray<double> ArrayDb;
+	std::ostream& arr_out(std::ostream& os) const;
+
+public:
+	Student() : std::string("Null Student"), ArrayDb() {}
+	explicit Student(const std::string& s)
+		: std::string(s), ArrayDb() {}
+	explicit Student(int n) : std::string("Nully"), ArrayDb(n) {}
+	Student(const std::string& s, int n)
+		: std::string(s), ArrayDb(n) {}
+	Student(const std::string& s, const ArrayDb& a)
+		: std::string(s), ArrayDb(a) {}
+	Student(const char* str, const double* pd, int n)
+		: std::string(str), ArrayDb(pd, n) {}
+	~Student() {}
+	double Average() const;
+	const std::string& Name() const;
+	double& operator[](int i);
+	double operator[](int i) const;
+
+	friend std::istream& operator>>(std::istream& is,
+		Student& stu);
+	friend std::istream& getline(std::istream& is,
+		Student& stu);
+	friend std::ostream& operator<<(std::ostream& os,
+		const Student& stu);
+};
